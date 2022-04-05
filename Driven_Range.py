1a8a693d815f56fdbcbfe6fef6435f2da7ae7fba
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-04-05 22:28:13 +0530

    Added code for  basic test

--- Driven_Range.py
+++ Driven_Range.py
@@ -1,4 +1,44 @@
 class driven_range:
 
-    def processedOutput(self, inputData):
-        return 0
+    def main(self, inputData):
+        inputData.sort()
+        self.inputData = inputData.copy()
+        return self.generateResult()
+
+  
+    def getRangeListInfo(self):
+        cumulativeFrequency = 0
+        rangeInfoList = []
+        listCurrentPosition = 0
+        while (cumulativeFrequency != len(self.inputData)):
+            rangeOpenerElement = self.inputData[listCurrentPosition]
+            rangeCloserPosition = self.getRangeCloserPosition(listCurrentPosition)
+            frequency = rangeCloserPosition - listCurrentPosition + 1
+            rangeCloserElement = self.inputData[rangeCloserPosition]
+            rangeInfoList.append((rangeOpenerElement, rangeCloserElement, frequency))
+            listCurrentPosition = rangeCloserPosition + 1
+            cumulativeFrequency+=frequency
+        return rangeInfoList
+
+
+    def getRangeCloserPosition(self, listBeginPosition):
+        rangeCloserPosition = listBeginPosition
+        for i in range(listBeginPosition+1, len(self.inputData)):
+            if((self.inputData[i]-self.inputData[rangeCloserPosition]) == 1):
+                rangeCloserPosition = i
+            else:
+                break
+        return rangeCloserPosition
+    
+    def generateResult(self):
+        rangeInfoList = self.getRangeListInfo()
+        rangeResult = {}
+        for rangeInfo in rangeInfoList:
+            rangeData = f'{rangeInfo[0]}-{rangeInfo[1]}'
+            freqData = f'{rangeInfo[2]}'
+            rangeResult.update({rangeData: freqData})
+            self.printOnConsole(f'{rangeData}, {freqData}')
+        return rangeResult
+    
+    def printOnConsole(self, rangeResult):
+        print(rangeResult)
--- Driven_Range_Test.py
+++ Driven_Range_Test.py
@@ -2,9 +2,9 @@ from Driven_Range import *
 import unittest
 
 class driven_range_test(unittest.TestCase):
+
     def test_rangeAndFrequency(self):
-        self.assertTrue(driven_range().processedOutput([4,5]) == {'4-5': '2'})
+        self.assertTrue(driven_range().main([4,5]) == {'4-5': '2'})
     
-#if __name__ == "__main__":
-#    driven_range_test().rangeAndFrequency()
+
 unittest.main()
