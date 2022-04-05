ae2ab980da2f54ecd089af8034d3edb25d3ad537
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-04-05 23:24:03 +0530

    Added basic test - 2

--- Driven_Range_Test.py
+++ Driven_Range_Test.py
@@ -5,6 +5,7 @@ class driven_range_test(unittest.TestCase):
 
     def test_rangeAndFrequency(self):
         self.assertTrue(driven_range().main([4,5]) == {'4-5': '2'})
+        self.assertTrue(driven_range().main([4,5,6,4]) == {'4-6': '4'})
     
 
 unittest.main()
